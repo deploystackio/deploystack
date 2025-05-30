82ff531b801e2a3c785b179809599342e42da534
Lasim <hajdas@hajdas.de>
2025-05-30 23:23:45 +0200

    fix: update conventional changelog plugin configuration for backend and frontend

--- services/frontend/.release-it.js
+++ services/frontend/.release-it.js
@@ -21,10 +21,12 @@ module.exports = {
   },
   "plugins": {
     "@release-it/conventional-changelog": {
-      "preset": "angular",
+      "preset": {
+        "name": "angular"
+      },
       "infile": "CHANGELOG.md",
       "ignoreRecommendedBump": true,
-      "commitPath": ".",
+      "path": "services/frontend",
       "writerOpts": {
         "commitsFilter": ["feat", "fix", "perf", "revert"],
         "transform": function(commit) {
