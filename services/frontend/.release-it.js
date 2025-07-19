68bdbc85564c1d8ce9731c22446261ca45924e44
Lasim <hajdas@hajdas.de>
2025-07-19 18:15:30 +0200

    fix: add newline to commitPartial format for better readability

--- services/frontend/.release-it.js
+++ services/frontend/.release-it.js
@@ -46,7 +46,7 @@ module.exports = {
 
           return newCommit;
         },
-        "commitPartial": "* {{subject}} ([{{shortHash}}](https://github.com/deploystackio/deploystack/commit/{{hash}}))"
+        "commitPartial": "* {{subject}} ([{{shortHash}}](https://github.com/deploystackio/deploystack/commit/{{hash}}))\n"
       }
     }
   }
