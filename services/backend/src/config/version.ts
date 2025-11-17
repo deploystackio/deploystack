1c71e34ffb0760082ae4694588ff5979de8139f0
Lasim <7317318+Lasim@users.noreply.github.com>
2025-11-17 21:05:29 +0000

    chore(backend): release v0.44.0

--- services/backend/src/config/version.ts
+++ services/backend/src/config/version.ts
@@ -9,8 +9,8 @@ export interface VersionInfo {
 
 // This will be replaced by the build script
 let versionData: VersionInfo = {
-  version: '0.43.0',
-  buildTime: '2025-11-15T13:42:38.756Z',
+  version: '0.44.0',
+  buildTime: '2025-11-17T21:05:29.108Z',
   source: 'release'
 };
 
