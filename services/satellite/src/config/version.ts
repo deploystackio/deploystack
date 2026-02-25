80088b5982a23358aff954a03ee7368061af2328
Lasim <7317318+Lasim@users.noreply.github.com>
2026-02-25 20:16:30 +0000

    chore(satellite): release v0.22.1

--- services/satellite/src/config/version.ts
+++ services/satellite/src/config/version.ts
@@ -9,8 +9,8 @@ export interface VersionInfo {
 
 // This will be replaced by the build script
 let versionData: VersionInfo = {
-  version: '0.22.0',
-  buildTime: '2026-02-24T22:59:36.234Z',
+  version: '0.22.1',
+  buildTime: '2026-02-25T20:16:29.734Z',
   source: 'release'
 };
 
